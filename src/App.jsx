22c13bca7e7a0550e0bed8ef945f31ea00a8c590
HarshDev2003 <harshkumardewangan2003@gmail.com>
2025-02-12 08:22:32 +0530

    Timelines

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,51 @@
 // import React from 'react'
-import { BrowserRouter,Routes, Route } from "react-router";
-import  { NavbarDefault } from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router";
+import { NavbarDefault } from "./components/Navbar";
 import Home from "./pages/Home";
 import TrueFocus from "./components/Preloader";
+import Steps from "./pages/Steps";
 
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      
-      <Route
-        path="/"
-        element={
-          <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <NavbarDefault />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/preloader"
+          element={
+            <>
+              <TrueFocus
+                sentence="True Focus"
+                manualMode={false}
+                blurAmount={5}
+                borderColor="red"
+                animationDuration={2}
+                pauseBetweenAnimations={1}
+              />
+            </>
+          }
+        />
+         <Route
+          path="/timeline"
+          element={
+            <>
             <NavbarDefault />
-            <Home />
-        
-          </>
-        }
-      />
-     <Route
-        path="/preloader"
-        element={
-          <>
-           <TrueFocus 
-sentence="True Focus"
-manualMode={false}
-blurAmount={5}
-borderColor="red"
-animationDuration={2}
-pauseBetweenAnimations={1}
-/>
-            
-        
-          </>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
-  )
+            <Steps />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
